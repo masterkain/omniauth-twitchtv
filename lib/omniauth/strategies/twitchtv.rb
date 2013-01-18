28f4affe530298946c2f03514a969d9effbcdf5c
Joel Jackson <jackson.joel@gmail.com>
2013-01-18 12:38:33 -0800

    Fills in the info hash for omniauth.

--- lib/omniauth/strategies/twitchtv.rb
+++ lib/omniauth/strategies/twitchtv.rb
@@ -13,6 +13,25 @@ module OmniAuth
       option :authorize_params, {}
       option :authorize_options, [:scope, :response_type]
       option :response_type, 'code'
+
+      def raw_info
+        get_hash_from_channel = lambda do |token|
+          http_client = HTTPClient.new
+          header = {"Authorization"=>"OAuth #{token}"}
+          http_client.get("https://api.twitch.tv/kraken/channel", "", header)
+        end
+        @channel_info || JSON.parse(get_hash_from_channel.call(access_token.token).body)
+      end
+
+      uid do
+        access_token.token
+      end
+
+      info do
+        {
+          name: raw_info["name"]
+        }
+      end
     end
   end
 end
