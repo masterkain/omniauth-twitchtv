e8f5ce3e52e65935ac6a1f23e4738c96085ee45c
Joel Jackson <jackson.joel@gmail.com>
2013-01-18 13:39:35 -0800

    Requests user information from the correct url for the scope requested

--- lib/omniauth/strategies/twitchtv.rb
+++ lib/omniauth/strategies/twitchtv.rb
@@ -14,15 +14,6 @@ module OmniAuth
       option :authorize_options, [:scope, :response_type]
       option :response_type, 'code'
 
-      def raw_info
-        get_hash_from_channel = lambda do |token|
-          http_client = HTTPClient.new
-          header = {"Authorization"=>"OAuth #{token}"}
-          http_client.get("https://api.twitch.tv/kraken/channel", "", header)
-        end
-        @channel_info || JSON.parse(get_hash_from_channel.call(access_token.token).body)
-      end
-
       uid do
         access_token.token
       end
@@ -31,6 +22,28 @@ module OmniAuth
         {
           name: raw_info["name"]
         }
+      end
+
+      def raw_info
+        get_hash_from_channel = lambda do |token|
+          http_client = HTTPClient.new
+          header = {"Authorization"=>"OAuth #{token}"}
+          http_client.get(info_url, "", header)
+        end
+        @raw_info ||= JSON.parse(get_hash_from_channel.call(access_token.token).body)
+      end
+
+      def info_url
+        url = if self.options.scopes && (self.options.scopes.index("user_read") || self.options.scopes.index(:user_read)) then
+          "https://api.twitch.tv/kraken/user"
+        elsif self.options.scopes && (self.options.scopes.index("channel_read") || self.options.scopes.index(:channel_read)) then
+          "https://api.twitch.tv/kraken/channel"
+        elsif self.options.scope.to_sym == :user_read then
+          "https://api.twitch.tv/kraken/user"
+        elsif self.options.scope.to_sym == :channel_read then
+          "https://api.twitch.tv/kraken/channel"
+        end
+        url
       end
     end
   end
